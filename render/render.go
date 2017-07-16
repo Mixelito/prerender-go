b6e25e0f02f77f8531fc8a345aceea184b9c5695
Michel Pinzetta Gayeski <michel@reclameaqui.com.br>
2017-07-15 21:32:06 -0300

    error stop loading

--- render/render.go
+++ render/render.go
@@ -221,7 +221,7 @@ func (r *chromeRenderer) Render(req *http.Request) (*Result, error) {
 	time.AfterFunc(PAGE_LOAD_TIMEOUT, func(){
 		if _, err = tab.Page.StopLoading(); err != nil {
 			//log.Fatal("error stop loading: "+err.Error())
-			log.Printf("error stop loading: %s", err)
+			log.Printf("error stop loading: %s : %s", err, url)
 		}
 		wg.Done()
 		res.Status = http.StatusGatewayTimeout
